1242390d70dee87baacd333b384f5182f4c1b64f
ohidulalam <mdnoyonctg15@gmail.com>
2021-10-31 21:31:32 +0600

    final BD setup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
